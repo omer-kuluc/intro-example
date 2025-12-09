e9320e4b633e704293fd56c0fa751f9e2f4365b6
omer-kuluc <omerkuluc96@gmail.com>
2025-12-09 17:43:53 +0300

    süre ayarları güncellendi

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-gsap.to(".intro-text", { text: "SKY&SEA STUDIO", ease: "power2.inOut", opacity: 1, duration: 2.5, repeat: 1, yoyo: true }, "+=2");
+gsap.to(".intro-text", { text: "SKY&SEA STUDIO", ease: "power2.inOut", opacity: 1, duration: 2.5, repeat: 1, yoyo: true });
 
 
 gsap.set(".effect", { autoAlpha: 1 })
 let tl = gsap.timeline()
-  .from("h1", { scale: 0.01, duration: 4, ease: "power4.inOut" }, "+=4.75")
+  .from("h1", { scale: 0.01, duration: 4, ease: "power4.inOut" }, "+=2.75")
   .to(".blendImage, .bg", { scale: 1, duration: 3.5 }, "-=3.75")
   .to(".dark", { opacity: 0, duration: 2 }, ">-=100%")
