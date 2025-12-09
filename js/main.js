03b9588fb6b1757e33f200d2640bc47cdd68d7d2
omer-kuluc <omerkuluc96@gmail.com>
2025-12-09 09:36:53 +0300

    main.js dosyasında düzenlemeler yapıldı

--- js/main.js
+++ js/main.js
@@ -1,8 +1,35 @@
-gsap.to(".intro-text", { text: "SKY&SEA STUDIO", ease: "power2.inOut", opacity: 1, duration: 2.5, repeat: 1, yoyo: true });
+// Sayfa yüklendikten sonra başlangıçta görünürlük ve opaklık ayarlarını yapıyoruz
+gsap.set(".effect", { opacity: 0, visibility: 'hidden' });
 
+// Hızlıca intro metni animasyonu yapıyoruz
+gsap.to(".intro-text", {
+  text: "SKY&SEA STUDIO",
+  ease: "power2.inOut",
+  opacity: 1,
+  duration: 2.5,
+  repeat: 1,
+  yoyo: true
+});
 
-gsap.set(".effect", { autoAlpha: 1 })
+// Zaman çizelgesi oluşturuyoruz
 let tl = gsap.timeline()
-  .from("h1", { scale: 0.01, duration: 4, ease: "power4.inOut" }, "+=2.75")
-  .to(".blendImage, .bg", { scale: 1, duration: 3.5 }, "-=3.75")
-  .to(".dark", { opacity: 0, duration: 2 }, ">-=100%")
+  .from("h1", {
+    scale: 0.01,
+    duration: 4,
+    ease: "power4.inOut"
+  }, "+=2.75")
+  .to(".blendImage, .bg", {
+    scale: 1,
+    duration: 3.5
+  }, "-=3.75")
+  .to(".dark", {
+    opacity: 0,
+    duration: 2
+  }, ">-=100%");
+
+// .effect öğesinin görünürlük ve opaklık animasyonu
+gsap.to(".effect", {
+  opacity: 1,
+  visibility: 'visible',
+  duration: 1
+});
